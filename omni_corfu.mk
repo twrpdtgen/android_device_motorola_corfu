d807d78e6ca12e82765d9e56d32088d6fcf365f5
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-11-23 22:18:03 +0100

    corfu: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: motorola Model: moto g41 Script version: 3.0.0

---
+++ omni_corfu.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from corfu device
+$(call inherit-product, device/motorola/corfu/device.mk)
+
+PRODUCT_DEVICE := corfu
+PRODUCT_NAME := omni_corfu
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g41
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="corfu_g-user 12 S3RWS32.138-29-5-11 aa6fb release-keys"
+
+BUILD_FINGERPRINT := motorola/corfu_g/corfu:12/S3RWS32.138-29-5-11/aa6fb:user/release-keys
